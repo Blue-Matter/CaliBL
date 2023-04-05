0a82e634f0922723c9c5c34378fcdfc15b7332f6
tcarruth <tomsinbox@gmail.com>
2023-04-05 09:00:24 -0700

    fix DemoOM

--- docs/copyfiles.R
+++ docs/copyfiles.R
@@ -4,13 +4,13 @@ githubdir = "C:/Github/"
 githubdir = "C:/Users/tcarruth/Documents/Github/"
 frompath = paste0(githubdir,"CDFW_Bag_Limits")
 frompathG = "G:/Shared drives/BM shared/1. Projects/CDFW_halibut_bag_limits"
-topath = paste0(githubdir,"CDFW_Bag_Limits_IO/docs") 
+topath = paste0(githubdir,"CaliBL/docs")
 
 copyall = function(thisdir, todir){
   fromfiles = list.files(thisdir,include.dirs = F)
   for(j in 1:length(fromfiles))  file.copy(paste0(thisdir,fromfiles[j]), paste0(todir,fromfiles[j]),overwrite=T)
-}  
-  
+}
+
 copyall(paste0(frompathG,"/References/"), paste0(topath,"/References/"))
 copyall(thisdir=paste0(frompath,"/Figures/Data/"),todir= paste0(topath,"/Figures/"))
 copyall(thisdir=paste0(frompath,"/Figures/Demo/"),todir= paste0(topath,"/Figures/"))
